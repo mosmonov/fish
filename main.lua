3465f0bf025558f0ac7402a91c5a68affef245f9
Maksat Osmonov <mkstnani08@gmail.com>
2017-04-16 01:39:03 -0400

    changed the code

--- main.lua
+++ main.lua
@@ -2,35 +2,27 @@
 
 local physics = require( "physics" )
 physics.start()
-physics.setGravity(0, 8)
-physics.setDrawMode("hybrid")
+local w, h = display.contentWidth, display.contentHeight
+
+local joint
 
 local water = display.newImage( "water.png", 160, 160 )
-water.x, water.y = display.contentCenterX+100, 550
-water.rotation = 25
+
+physics.addBody( water, "static", { friction=0.1, bounce=0.1 } )
 
 local fish = display.newImage( "fish.png", 160, 240 )
-fish.x, fish.y = display.contentCenterX-70, 500
+physics.addBody( fish, { density=1.3, radius =10, friction=1} )
 
-physics.addBody( water, "static", { friction=0.2, bounce=0.3 } )
 
-local fish2 = display.newImage( "fish2.png", 150, 400 )
--- fish2.rotation = 1
+local fish2 = display.newImage( "fish.png", 150, 300 )
 
-physics.addBody( fish2, "static", { density=0, friction=0, bounce=0 } )
+physics.addBody( fish2, { density=0 } )
 
-joint = physics.newJoint("piston", water, fish, water.x, water.y, 0, 1)
+joint = physics.newJoint("pivot", fish2, water, fish, w/3 + 5, h - 100)
 
-joint.isLimitEnabled = true
-local p1, p2 = joint:getLimits()
-print(p1, p2)
-joint:setLimits(-240, 120)
-
-joint.isMotorEnabled = true
-joint.motorSpeed = -80
-joint.maxMotorForce = 1000
-
--- joint.isCollideConnected = false
+	joint.isMotorEnabled = true
+	joint.maxMotorTorque = 10000
+	joint.motorSpeed = 80
 
 
 
